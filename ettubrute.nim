d120dd7585b0fa2f038efc5099a2cdebb7c6a4b3
Ryan Emmons <75249740+the-emmon@users.noreply.github.com>
2021-05-11 19:47:52 -0500

    Version 1.1
    Added version numbers, speed parameter, and color output.

--- ettubrute.nim
+++ ettubrute.nim
@@ -1,24 +1,26 @@
 from os import sleep, splitPath; from uri import encodeUrl; from times import epochTime
-import httpclient, docopt, strutils, threadpool, system, avxcount
+import httpclient, docopt, strutils, threadpool, system, avxcount, terminal
 
 # banner, args, and variables
+enableTrueColors()
 let banner = """
-              .               .                        .o8                                .
-            .o8             .o8                       "888                              .o8
- .ooooo.  .o888oo         .o888oo oooo  oooo           888oooo.  oooo d8b oooo  oooo  .o888oo  .ooooo.
-d88' `88b   888             888   `888  `888           d88' `88b `888""8P `888  `888    888   d88' `88b
-888ooo888   888   8888888   888    888   888  8888888  888   888  888      888   888    888   888ooo888
-888    .o   888 .           888 .  888   888           888   888  888      888   888    888 . 888    .o
-`Y8bod8P'   "888"           "888"  `V88V"V8P'          `Y8bod8P' d888b     `V88V"V8P'   "888" `Y8bod8P'
-
-                                     It's March 15th, Julius CSRF.
+               .               .                        .o8                                .             
+             .o8             .o8                       "888                              .o8             
+  .ooooo.  .o888oo         .o888oo oooo  oooo           888oooo.  oooo d8b oooo  oooo  .o888oo  .ooooo.  
+ d88' `88b   888             888   `888  `888           d88' `88b `888""8P `888  `888    888   d88' `88b 
+ 888ooo888   888   8888888   888    888   888  8888888  888   888  888      888   888    888   888ooo888 
+ 888    .o   888 .           888 .  888   888           888   888  888      888   888    888 . 888    .o 
+ `Y8bod8P'   "888"           "888"  `V88V"V8P'          `Y8bod8P' d888b     `V88V"V8P'   "888" `Y8bod8P' 
+                                                                                                         
+                                      It's March 15th, Julius CSRF.                                      
+                                             Version 1.1                                                 
 """
 let doc = """
-Usage: ettubrute <login_url> <csrf_htmlname> <wordlist> <postdata_nocsrf> <username> <errormsg>
+Usage: ettubrute <login_url> <csrf_htmlname> <wordlist> <postdata_nocsrf> <username> <errormsg> [--speed <spd>]
 
-It's March 15th, Julius CSRF.
-
-Postdata should have '^USER^' and '^PASS^' substituted in for fuzz positioning."""
+Options:
+  --speed=<spd>  Bruteforce speed, 1-5 [default: 5]."""
+let usage = "\nUsage: ettubrute <login_url> <csrf_htmlname> <wordlist> <postdata_nocsrf> <username> <errormsg> [--speed=<spd>]"
 
 var
   login_url: string
@@ -27,42 +29,103 @@ var
   wordlist: string
   username: string
   errormsg: string
-  timetaken = epochTime()
-  args      = docopt(doc)
-  count     = 0
+  brutespeed: int
+  timetaken* = epochTime()
+  args       = docopt(doc, version = banner)
+  sleeptime  = 2
+  count*     = 0
+  cCount*    = 0
 
 # There's a better way to validate variables using docopt, but I'm fine with this.
 try:
-  login_url = $args["<login_url>"]
+  login_url  = $args["<login_url>"]
   token_name = $args["<csrf_htmlname>"]
-  postdata = $args["<postdata_nocsrf>"]
-  wordlist = $args["<wordlist>"]
-  username = $args["<username>"]
-  errormsg = $args["<errormsg>"]
+  wordlist   = $args["<wordlist>"]
+  username   = $args["<username>"]
+  errormsg   = $args["<errormsg>"]
 except:
+  setBackgroundColor(bgBlack)
+  setForegroundColor(fgWhite, bright=true)
   echo banner
+  resetAttributes()
   echo doc
   quit()
+
+try: brutespeed = parseInt($args["--speed"])
+except:
+  setBackgroundColor(bgBlack)
+  setForegroundColor(fgWhite, bright=true)
+  echo banner
+  resetAttributes()
+  echo doc
+  quit()
+if brutespeed >= 6:
+  setBackgroundColor(bgBlack)
+  setForegroundColor(fgWhite, bright=true)
+  echo banner
+  resetAttributes()
+  echo doc
+  quit()
+elif brutespeed == 5: discard
+elif brutespeed == 4: sleeptime = 10
+elif brutespeed == 3: sleeptime = 20
+elif brutespeed == 2: sleeptime = 50
+elif brutespeed == 1: sleeptime = 100
 
 # Verify that we know where to inject username and password.
-if "^USER^" in postdata == false or "^PASS^" in postdata == false:
+try: postdata   = $args["<postdata_nocsrf>"]
+except:
+  setBackgroundColor(bgBlack)
+  setForegroundColor(fgWhite, bright=true)
   echo banner
+  resetAttributes()
   echo doc
+  echo "Postdata seems to be invalid."
+if "^USER^" in postdata == false or "^PASS^" in postdata == false or token_name in postdata:
+  setBackgroundColor(bgBlack)
+  setForegroundColor(fgWhite, bright=true)
+  echo banner
+  resetAttributes()
+  write(stdout, " ")
+  setBackgroundColor(bgBlack)
+  setForegroundColor(fgWhite, bright = true)
+  write(stdout, "ERROR ")
+  resetAttributes()
+  write(stdout, " - Argument 'postdata' must include '^USER^' and '^PASS^', and the token name shouldn't be included.\n")
   quit()
 
-# Trap ctrl+c, initialize program
-proc handler() {.noconv.} =
-  echo "\n[!] Caught CTRL+C [!]\n--- Ending bruteforce ----"
-  sync()
-  echo("\nRan for " & split($formatFloat(epochTime() - timetaken), ".")[0] & " seconds, tried " & $count & " passwords")
-  quit()
+# Trap ctrl+c
+proc ctrlc() {.noconv.} =
+  if cCount == 1:
+    write(stdout, " ")
+    setBackgroundColor(bgBlack)
+    setForegroundColor(fgWhite, bright = true)
+    write(stdout, "Caught CTRL+C")
+    resetAttributes()
+    write(stdout, "\n")
+    echo("Stopped before bruteforce finished.\nRan for " & split($formatFloat(epochTime() - timetaken), ".")[0] & " seconds, tried " & $count & " passwords")
+    quit()
+  else:
+    cCount = 1
+    write(stdout, " ")
+    setBackgroundColor(bgBlack)
+    setForegroundColor(fgWhite, bright = true)
+    write(stdout, "Caught CTRL+C")
+    resetAttributes()
+    write(stdout, "\n")
+    sync()
+    echo("Stopped before bruteforce finished\nRan for " & split($formatFloat(epochTime() - timetaken), ".")[0] & " seconds, tried " & $count & " passwords")
+    quit()
 
+setControlCHook(ctrlc)
+
+# Initialize program
+setBackgroundColor(bgBlack)
+setForegroundColor(fgWhite, bright=true)
 echo banner
-setControlCHook(handler)
-var
-  linecount = parseInt($fastCount(wordlist)) # Ultrafast linecount for large files using avxcount.nim by aboisvert
-  sleeptime = 2 # Change this to a higher number if you're getting filesystem errors or if the webserver is very slow.
-echo("Starting file " & splitPath(wordlist).tail & ", " & $linecount & " lines.\n")
+resetAttributes()
+var linecount = parseInt($fastCount(wordlist)) # Ultrafast linecount for large files using avxcount.nim by aboisvert
+echo("Starting file " & splitPath(wordlist).tail & ", " & $linecount & " lines:")
 
 # Define the procedure our threads will run
 proc main(loginURL:string,tokenName:string,postData:string,uname:string,pword:string,errorMsg:string,linenum:int):string {.thread.} =
@@ -83,7 +146,11 @@ proc main(loginURL:string,tokenName:string,postData:string,uname:string,pword:st
     response     = http.request(loginURL & "?" & inputData, httpMethod = HttpPost, headers = cookieHeader) # Try password
   echo("  --> " & pword)
   if errorMsg in response.body == false and "field is required." in response.body == false:
-    echo("Password seems to have been found! -> " & pword)
+    setForegroundColor(fgBlack, bright=true)
+    setBackgroundColor(bgGreen)
+    write(stdout, "Password seems to have been found! -> " & pword)
+    resetAttributes()
+    write(stdout, "\n")
     quit()
   close(http)
 
